a41eb521f928632c35a5d54dcc71769f48780827
Andy Isaev <66003375+AndIsaev@users.noreply.github.com>
2021-03-12 23:54:49 +0300

    Update settings.py

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -81,6 +81,16 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get('DB_ENGINE'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+    }
+}
 
 
 REST_FRAMEWORK = {
