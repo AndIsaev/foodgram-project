b9f0aed427337aa2c1cf18432101870f1a31a2da
Andrey <and.isaev.v@yandex.ru>
2021-03-15 11:44:04 +0300

    success_reg

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("signup/", views.SignUp.as_view(), name="signup")
+    path("signup/", views.SignUp.as_view(), name="signup"),
+    path("success_reg", views.success_reg, name = "success_reg")
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from .forms import CreationForm
@@ -5,5 +6,8 @@ from .forms import CreationForm
 
 class SignUp(CreateView):
     form_class = CreationForm
-    success_url = reverse_lazy("signup")
+    success_url = reverse_lazy("success_reg")
     template_name = "signup.html"
+
+def success_reg(request):
+    return render(request, "registration/success_reg.html")
