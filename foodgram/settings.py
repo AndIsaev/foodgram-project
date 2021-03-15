8daf637e3124d7062a9fe643d5a3e9aa22ed35b6
Andy Isaev <66003375+AndIsaev@users.noreply.github.com>
2021-03-15 23:21:08 +0300

    Update settings.py

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -92,13 +92,6 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
